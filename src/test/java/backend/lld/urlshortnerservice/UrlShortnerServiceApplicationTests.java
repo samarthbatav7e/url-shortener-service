45ae57cf728adec3652dff851d91aefae93422ee
Samarth Batav <samarthbatav7@gmail.com>
2026-07-10 00:12:59 +0530

    Project Structure for url-shotener-service pushed to github.

---
+++ src/test/java/backend/lld/urlshortnerservice/UrlShortnerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.lld.urlshortnerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlShortnerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
