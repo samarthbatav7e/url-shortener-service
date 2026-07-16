c72c9771306cae43ac727b20f15752acbd4954bf
Samarth Batav <samarthbatav7@gmail.com>
2026-07-16 23:37:47 +0530

    Converted interface UrlMappingRepository into a Spring Data Repository.

--- src/main/java/com/lld/urlshortnerservice/url/repository/UrlMappingRepository.java
+++ src/main/java/com/lld/urlshortnerservice/url/repository/UrlMappingRepository.java
@@ -1,15 +1,16 @@
 package com.lld.urlshortnerservice.url.repository;
 
 import com.lld.urlshortnerservice.url.entity.UrlMapping;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UrlMappingRepository {
+public interface UrlMappingRepository extends JpaRepository<UrlMapping,Long> {
 
     UrlMapping save(UrlMapping urlMapping);
     Optional<UrlMapping>findByLongUrl(String longUrl);
     Optional<UrlMapping>findByShortCode(String shortCode);
     boolean existsByShortCode(String shortCode);
-
+    boolean existsByLongUrl(String longUrl);
 
 }
